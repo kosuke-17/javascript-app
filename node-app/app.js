b6667b796335322379405f819edcce2b899f4669
kosuke-17 <tamutamu17@icloud.com>
2021-04-19 18:14:28 +0900

    first commit

---
+++ node-app/app.js
@@ -0,0 +1,17 @@
+const http = require('http');
+
+const server = http.createServer(
+  (request,response)=>{
+    response.setHeader('Content-Type', 'text/html');
+    response.write('<!DOCTYPE html><html lang="ja">');
+    response.write('<head><meta charset="utf-8">');
+    response.write('<title>Hello</title></head>');
+    response.write('<body><h1>Hello NOde.Js!</h1>');
+    response.write('<p>This is Node.Js sample page.</p>');
+    response.write('<p>これは Node.jsのサンプルページです。</p>', 'utf8');
+    response.write('</body></html>');
+    response.end();
+  }
+);
+server.listen(3000);
+console.log('Server start!');
