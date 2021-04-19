f5b24ff3b2be943740436a8498bb85862a00a3f6
kosuke-17 <tamutamu17@icloud.com>
2021-04-19 21:50:03 +0900

    app.jsで文字列をejsに表示

--- node-app/app.js
+++ node-app/app.js
@@ -13,7 +13,10 @@ console.log('Server start!');
 
 // createServerの処理
 function getFromClient(request, response) {
-  var content = ejs.render(index_page);
+  var content = ejs.render(index_page, {
+    title: "Indexページ",
+    content: "これはテンプレートを使ったサンプルページです。",
+  });
   response.writeHead(200, { 'Content-Type': 'text/html' });
   response.write(content);
   response.end();
