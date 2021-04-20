47e5884403c75a8696792329217599718d25fb81
kosuke-17 <tamutamu17@icloud.com>
2021-04-20 13:42:23 +0900

    クエリパラメータ表示とフォーム送信の実装途中

--- node-app/app.js
+++ node-app/app.js
@@ -2,6 +2,7 @@ const http = require('http');
 const fs =require('fs');
 const ejs = require('ejs');
 const url = require('url');
+const qs = require('querystring');
 
 const index_page = fs.readFileSync('./index.ejs', 'utf8');
 const other_page = fs.readFileSync('./other.ejs', 'utf8');
@@ -17,13 +18,18 @@ console.log('Server start!');
 // createServerの処理
 function getFromClient(request, response) {
 
-  var url_parts = url.parse(request.url);
+  var url_parts = url.parse(request.url, true);
   switch (url_parts.pathname) {
 
     case '/':
+      var content = "これはIndexページです。"
+      var query = url_parts.query;
+      if (query.msg != undefined) {
+          content += 'あなたは、｢' + query.msg + '｣と送りました'
+      }
       var content = ejs.render(index_page, {
         title: "Index",
-        content: "これはテンプレートを使ったサンプルページです。",
+        content: content,
       });
       response.writeHead(200, { 'Content-Type': 'text/html' });
       response.write(content);
