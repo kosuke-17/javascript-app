c8d819633a5969ea9cdba5eb1e16068d01a87c07
kosuke-17 <tamutamu17@icloud.com>
2021-04-20 15:43:27 +0900

    伝言メッセージ機能追加

--- node-app/app.js
+++ node-app/app.js
@@ -41,22 +41,38 @@ function getFromClient(request, response) {
     break;
   }
 }
-var data = {
-  'Taro': '09-999-999',
-  'Hanako': '080-888-888',
-  'Sachiko': '070-777-777',
-  'Ichiro': '060-666-666'
-};
+var data = { msg: 'no message...' };
 
 // indexのアクセス処理
 function response_index(request, response) {
-  var msg = "これはIndexページです。"
-  var content = ejs.render(index_page, {
-    title: "Index",
-    content: msg,
-    data: data,
-    filename: 'data_item'
-  });
+  //POSTアクセス時の処理
+  if (request.method == 'POST') {
+    var body = '';
+
+    //データ受信のイベント処理
+    request.on('data', (data) =>{
+      body += data;
+    });
+
+    //データ受信終了のイベント処理
+    request.on('end', () => {
+      data = qs.parse(body);
+      write_index(request, response);
+    });
+  } else {
+    write_index(request, response);
+  }
+}
+
+//indexの表示作成
+function write_index(request, response) {
+  var msg = "※伝言を表示します。"
+  var content = ejs.render(
+    index_page, {
+      title: "Index",
+      content: msg,
+      data: data,
+  })
   response.writeHead(200, { 'Content-Type': 'text/html' });
   response.write(content);
   response.end();
@@ -81,3 +97,4 @@ function response_other(request,response) {
   response.writeHead(200, { 'Content-Type': 'text/html' });
   response.write(content);
   response.end();
+}
