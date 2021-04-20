77fdf42838f89d4095442d3e05551fe0b21bd09a
kosuke-17 <tamutamu17@icloud.com>
2021-04-20 16:02:10 +0900

    クッキー利用の処理

--- node-app/app.js
+++ node-app/app.js
@@ -57,6 +57,8 @@ function response_index(request, response) {
     //データ受信終了のイベント処理
     request.on('end', () => {
       data = qs.parse(body);
+      //クッキーの保存
+      setCookie('msg', data.msg, response);
       write_index(request, response);
     });
   } else {
@@ -67,11 +69,12 @@ function response_index(request, response) {
 //indexの表示作成
 function write_index(request, response) {
   var msg = "※伝言を表示します。"
-  var content = ejs.render(
-    index_page, {
+  var cookie_data = getCookie('msg', request);
+  var content = ejs.render(index_page, {
       title: "Index",
       content: msg,
       data: data,
+      cookie_data: cookie_data,
   })
   response.writeHead(200, { 'Content-Type': 'text/html' });
   response.write(content);
@@ -84,6 +87,25 @@ var data2 = {
   'Sachiko': ['sachi@happy','070-777-777', 'Nagoya'],
   'Ichiro': ['ichi@baseball','060-666-666', 'USA'],
 };
+
+//クッキーの値を設定
+function setCookie(key, value, response) {
+  var cookie = escape(value);
+  response.setHeader('Set-Cookie', [key + '=' + cookie]);
+}
+//クッキーの値を取得
+function getCookie(key, request) {
+  var cookie_data = request.headers.cookie != undefined ?
+    request.headers.cookie : '';
+  var data = cookie_data.split(';');
+  for (var i in data) {
+    if (data[i].trim().startsWith(key + '=')) {
+      var result = data[i].trim().substring(key.length + 1);
+      return unescape(result);
+    }
+  }
+  return '';
+}
 
 // otherのアクセス処理
 function response_other(request,response) {
